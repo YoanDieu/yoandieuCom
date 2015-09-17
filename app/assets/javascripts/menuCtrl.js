390067392e066bbd0b4470f5d9a0240f2219ec20
YoanDieu <yoan.dieu@gmail.com>
2015-09-17 21:06:57 +0200

    made some modification to fix the bug who cause the right menu not to close when windows get larger enough to make the burgerBtn disapear

--- app/assets/javascripts/menuCtrl.js
+++ app/assets/javascripts/menuCtrl.js
@@ -1,15 +1,21 @@
 $burgerBtn = document.getElementById("burgerBtn");
 $rightMenu = document.getElementById("rightMenu");
+$topMenu = document.getElementById("topMenu");
+$link1 = document.getElementById("link1");
+$link2 = document.getElementById("link2");
+$link3 = document.getElementById("link3");
 
 function toggleMenu() {
-  menuState = getComputedStyle($rightMenu).display;
-
-  if (menuState == "block"){
+  rightMenuState = getComputedStyle($rightMenu).display;
+  topMenuState = getComputedStyle($topMenu.firstChild.nextSibling).display;
+  if (rightMenuState == "block" && topMenuState != "none"){
     $rightMenu.style.display = "none";
+
   } else {
     $rightMenu.style.display = "block";
   }
 
   }
+
 
   $burgerBtn.addEventListener('click', toggleMenu, false);
