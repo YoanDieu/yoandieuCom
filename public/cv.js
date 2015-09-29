5fc96ffca139ca2108994f43946b936acdf06902
YoanDieu <yoan.dieu@gmail.com>
2015-09-29 23:35:23 +0200

    fixed the animation, trying some buttons effect in javascript, to let the user see on what slide he is

--- public/cv.js
+++ public/cv.js
@@ -1,5 +1,3 @@
-alert('test');
-
 $formationsBtn = document.getElementById("formationsBtn");
 $experiencesBtn = document.getElementById("experiencesBtn");
 $activitesBtn = document.getElementById("activitesBtn");
@@ -7,25 +5,109 @@ $activitesBtn = document.getElementById("activitesBtn");
 $formations = document.getElementById("formations");
 $experiences = document.getElementById("experiences");
 $activites = document.getElementById("activites");
+$cvWarper = document.getElementById('cvWarper');
+
+if (getComputedStyle($formations).height > getComputedStyle($experiences).height || getComputedStyle($formations).height > getComputedStyle($activites).height) {
+  $cvWarper.style.minHeight = getComputedStyle($formations).height;
+} else if (getComputedStyle($experiences).height > getComputedStyle($formations).height ||getComputedStyle($experiences).height > getComputedStyle($activites).height) {
+  $cvWarper.style.minHeight = getComputedStyle($experiences).height;
+} else if (getComputedStyle($activites).height > getComputedStyle($formations).height || getComputedStyle($activites).height > getComputedStyle($experiences).height) {
+  $cvWarper.style.minHeight = getComputedStyle($activites).height;
+}
 
 function formationClick(){
   $formations.style.marginLeft = "0%";
+  $formations.style.opacity = "1";
   $experiences.style.marginLeft = "101%";
+  $experiences.style.opacity = "0";
   $activites.style.marginLeft = "201%";
+  $activites.style.opacity = "0";
+
+  $formationsBtn.style.backgroundColor = "white";
+  $formationsBtn.style.color = "#06aae7";
+
+  $experiencesBtn.style.backgroundColor = "#06aae7";
+  $experiencesBtn.style.color = "white";
+
+  $activitesBtn.style.backgroundColor = "#06aae7";
+  $activitesBtn.style.color = "white";
 }
 
 function experiencesClick(){
   $experiences.style.marginLeft = "0%";
+  $experiences.style.opacity = "1";
   $formations.style.marginLeft = "-101%";
-  $activites.style.marginLeft = "1%";
+  $formations.style.opacity = "0";
+  $activites.style.marginLeft = "101%";
+  $activites.style.opacity = "0";
+
+  $experiencesBtn.style.backgroundColor = "white";
+  $experiencesBtn.style.color = "#06aae7";
+
+  $formationsBtn.style.backgroundColor = "#06aae7";
+  $formationsBtn.style.color = "white";
+
+  $activitesBtn.style.backgroundColor = "#06aae7";
+  $activitesBtn.style.color = "white";
 }
 
 function activitesClick(){
-  $activites.style.marginLeft = "1%";
+  $activites.style.marginLeft = "0%";
+  $activites.style.opacity = "1";
+  $formations.style.marginLeft = "-201%";
+  $formations.style.opacity = "0";
   $experiences.style.marginLeft = "-101%";
-  $fromations.style.marginLeft = "-201%";
+  $experiences.style.opacity = "0";
+
+  $activitesBtn.style.backgroundColor = "white";
+  $activitesBtn.style.color = "#06aae7";
+
+  $formationsBtn.style.backgroundColor = "#06aae7";
+  $formationsBtn.style.color = "white";
+
+  $experiencesBtn.style.backgroundColor = "#06aae7";
+  $experiencesBtn.style.color = "white";
+
+}
+
+function formationHover(){
+  $formationsBtn.style.backgroundColor = "white";
+  $formationsBtn.style.color = "#06aae7";
+}
+
+function experiencesHover(){
+  $experiencesBtn.style.backgroundColor = "white";
+  $experiencesBtn.style.color = "#06aae7";
+}
+
+function activitesHover(){
+  $activitesBtn.style.backgroundColor = "white";
+  $activitesBtn.style.color = "#06aae7";
+}
+
+function formationOut(){
+  $formationsBtn.style.backgroundColor = "#06aae7";
+  $formationsBtn.style.color = "white";
+}
+
+function experiencesOut(){
+  $experiencesBtn.style.backgroundColor = "#06aae7";
+  $experiencesBtn.style.color = "white";
+}
+
+function activitesOut(){
+  $activitesBtn.style.backgroundColor = "#06aae7";
+  $activitesBtn.style.color = "white";
 }
 
 $formationsBtn.addEventListener('click', formationClick, false);
 $experiencesBtn.addEventListener('click', experiencesClick, false);
 $activitesBtn.addEventListener('click', activitesClick, false);
+
+$formationsBtn.addEventListener('mouseover', formationHover, false);
+$experiencesBtn.addEventListener('mouseover', experiencesHover, false);
+$activitesBtn.addEventListener('mouseover', activitesHover, false);
+
+$formationsBtn.addEventListener('mouseout', formationOut, false);
+$experiencesBtn.addEventListener('mouseout', experiencesOut, false);
+$activitesBtn.addEventListener('mouseout', activitesOut, false);
