903918457efb7f4d689e85e7e08ff7e64ff65fed
YoanDieu <yoan.dieu@gmail.com>
2015-10-19 17:48:12 +0200

    changed css for mediumlarge screen on home view

--- app/assets/javascripts/jumbotron.js
+++ app/assets/javascripts/jumbotron.js
@@ -1,6 +1,6 @@
 $jumbotron = document.getElementById("jumbotron");
 
 
-var jumbos = ["mars.jpeg","marscratere.jpg","rosetta.jpg","singularity.jpg","tore1.jpg","mir8.jpg","MARS-facebook.jpg","isscopula.jpg"];
+var jumbos = ["mars.jpeg","marscratere.jpg", "singularity.jpg", "tore1.jpg", "mir8.jpg", "MARS-facebook.jpg", "isscopula.jpg"];
 var random = parseInt(Math.random() * (jumbos.length - 0) + 0);
 $jumbotron.style.backgroundImage = "url(" + jumbos[random]+ ")";
