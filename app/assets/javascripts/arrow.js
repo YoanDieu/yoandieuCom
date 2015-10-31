6917782eda0a34820f6a6a6eb73e585209c5ff44
YoanDieu <yoan.dieu@gmail.com>
2015-10-31 17:57:53 +0100

    modify extand on textshadow

--- app/assets/javascripts/arrow.js
+++ app/assets/javascripts/arrow.js
@@ -38,7 +38,7 @@ $( document ).ready(function() {
   }, 500);
 
   setInterval(function(){
-    $arrow.style.textShadow = ""
+    $arrow.style.textShadow = "";
     $arrow.style.textShadow = "0px 0px 1px rgba(6,170,231,0)";
   }, 1000);
 
